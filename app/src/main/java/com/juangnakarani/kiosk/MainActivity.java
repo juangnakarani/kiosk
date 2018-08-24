6e4a6f51c760a914e98f4830ef55163a1f6863dd
juangnakarani <juang.nakarani@gmail.com>
2018-08-24 13:49:14 +0700

    request app permission

--- app/src/main/java/com/juangnakarani/kiosk/MainActivity.java
+++ app/src/main/java/com/juangnakarani/kiosk/MainActivity.java
@@ -1,10 +1,14 @@
 package com.juangnakarani.kiosk;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.content.ContextCompat;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -16,6 +20,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
@@ -94,6 +99,11 @@ public class MainActivity extends AppCompatActivity
             CURRENT_TAG = TAG_SALES;
             loadHomeFragment();
         }
+
+        //Request App Permission
+        ActivityCompat.requestPermissions(MainActivity.this,
+                new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                1);
 
     }
 
@@ -240,5 +250,29 @@ public class MainActivity extends AppCompatActivity
                 return new SalesFragment();
         }
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case 1: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+                } else {
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                    Toast.makeText(getApplicationContext(), "Permission denied to read your External storage", Toast.LENGTH_SHORT).show();
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request.
+        }
+    }
+
 
 }
--- app/src/main/java/com/juangnakarani/kiosk/preference/DialogBackupPreference.java
+++ app/src/main/java/com/juangnakarani/kiosk/preference/DialogBackupPreference.java
@@ -1,11 +1,22 @@
 package com.juangnakarani.kiosk.preference;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.pm.PackageManager;
+import android.os.Environment;
 import android.preference.DialogPreference;
+import android.support.v4.content.ContextCompat;
 import android.util.AttributeSet;
 import android.util.Log;
+import android.widget.Toast;
 
 import com.juangnakarani.kiosk.R;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.channels.FileChannel;
 
 public class DialogBackupPreference extends DialogPreference {
     public DialogBackupPreference(Context context, AttributeSet attrs) {
@@ -22,10 +33,40 @@ public class DialogBackupPreference extends DialogPreference {
         if (positiveResult) {
             // User selected OK
             Log.i("chk","bakup ok");
+
+            doBackup();
+
         } else {
             // User selected Cancel
             Log.i("chk","bakup cancel");
         }
     }
+
+    private void doBackup(){
+        Log.e("chk","doBackup()");
+        Log.e("chk",getContext().getPackageName());
+        File sd = Environment.getExternalStorageDirectory();
+        File data = Environment.getDataDirectory();
+        FileChannel source=null;
+        FileChannel destination=null;
+        String currentDBPath = "/data/"+ getContext().getPackageName() + "/databases/Kiosk.db";
+        String backupDBPath = "/Kiosbak.db";
+        File currentDB = new File(data, currentDBPath);
+        File backupDB = new File(sd, backupDBPath);
+        try {
+            source = new FileInputStream(currentDB).getChannel();
+            destination = new FileOutputStream(backupDB).getChannel();
+            destination.transferFrom(source, 0, source.size());
+            source.close();
+            destination.close();
+            Toast.makeText(getContext(), "Backup success!", Toast.LENGTH_SHORT).show();
+        } catch(IOException e) {
+            e.printStackTrace();
+            Log.e("chk","doBackup() IOException " + e);
+            Toast.makeText(getContext(), "Backup failed!", Toast.LENGTH_SHORT).show();
+
+        }
+    }
+
 
 }
